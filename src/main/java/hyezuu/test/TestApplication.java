bc3d764d4d7f720436a4ff2ef1a2717f6ff5f2f3
hyezuu <duudu0704@gmail.com>
2024-08-24 06:36:49 +0900

    feat(dependency): enablescheduled, 웹의존성추가

--- src/main/java/hyezuu/test/TestApplication.java
+++ src/main/java/hyezuu/test/TestApplication.java
@@ -2,7 +2,9 @@ package hyezuu.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TestApplication {
 

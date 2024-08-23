bd5ca5c3787cac099ad3922e3d73bd7ed8ab2e2c
hyezuu <duudu0704@gmail.com>
2024-08-24 06:35:44 +0900

    feat(common): 상수저장용클래스

---
+++ src/main/java/hyezuu/test/constant/Constant.java
@@ -0,0 +1,8 @@
+package hyezuu.test.constant;
+
+public class Constant {
+
+    public static final String DEFAULT_PROMPT =
+        "지금 가장 이슈가되는 해외토픽 기사 top5에서 A2에 적합한 영어 단어 30개를 vocabulary : , meaning : , example : 형태로 "
+            + "작성해줘, 줄바꿈은 가능하지만 그외의 마크다운 언어는 지양해줘.";
+}
